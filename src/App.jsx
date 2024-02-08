00bdcf32b1c2f1f38cc75aa4479e45f918697229
Ahmed Kamel <ahmedkamel.egy5577@gmail.com>
2024-02-08 03:56:28 +0200

    Trending section was finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
-import { fetchDataFromApi } from "./utils/api";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
+import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -14,16 +15,18 @@ import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
 	const dispatch = useDispatch();
-	// const { url } = useSelector((state) => state.home);
+	const { url } = useSelector((state) => state.home);
 	// console.log(url);
 
 	useEffect(() => {
-		fetchingApiConfig();
+		fetchApiConfig();
+		genresCall();
 	}, []);
 
-	const fetchingApiConfig = () => {
+	const fetchApiConfig = () => {
 		fetchDataFromApi("/configuration").then((res) => {
 			// console.log(res);
+
 			const url = {
 				backdrop: res.images.secure_base_url + "original",
 				poster: res.images.secure_base_url + "original",
@@ -32,6 +35,24 @@ function App() {
 
 			dispatch(getApiConfiguration(url));
 		});
+	};
+
+	const genresCall = async () => {
+		let promises = [];
+		let endPoints = ["tv", "movie"];
+		let allGenres = {};
+
+		endPoints.forEach((url) => {
+			promises.push(fetchDataFromApi(`/genre/${url}/list`));
+		});
+
+		const data = await Promise.all(promises);
+		console.log(data);
+		data.map(({ genres }) => {
+			return genres.map((item) => (allGenres[item.id] = item));
+		});
+		console.log(allGenres);
+		dispatch(getGenres(allGenres));
 	};
 
 	return (
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 
-// import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+
 import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carsoul/Carousel";
 
 const Trending = () => {
-	const onTabChange = (tab) => {};
+	const [endpoint, setEndpoint] = useState("day");
+
+	const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
+
+	const onTabChange = (tab) => {
+		setEndpoint(tab === "Day" ? "day" : "week");
+	};
 
 	return (
 		<div className="carouselSection">
@@ -14,7 +21,7 @@ const Trending = () => {
 				<span className="carouselTitle">Trending</span>
 				<SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
 			</ContentWrapper>
-			{/* <Carousel data={data?.results} loading={loading} /> */}
+			<Carousel data={data?.results} loading={loading} />
 		</div>
 	);
 };
