7afae435ebfd063dae42cd8d51858dfed47cbfd1
Ahmed Kamel <ahmedkamel.egy5577@gmail.com>
2024-02-09 01:59:09 +0200

    ..

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 const Home = () => {
 	return (
 		<>
 			<HeroBanner />
 			<Trending />
-			<div style={{ height: "100vh" }}></div>
+			<Popular />
+			<TopRated />
 		</>
 	);
 };
